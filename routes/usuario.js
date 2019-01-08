1b447258d58a2de58cc7a71623c7507858162569
José Sánchez Díez <jsanchezd@Itteria.com>
2019-01-08 09:53:53 +0100

    Añadido campo en get usuarios y quitada la obligacion de mdAutentificacion para crear usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -16,7 +16,7 @@ app.get('/', (req, res, next) => {
     var desde = req.query.desde || 0;
     desde = Number(desde);
 
-    Usuario.find({}, 'nombre email img role')
+    Usuario.find({}, 'nombre email img role google')
         .skip(desde)
         .limit(5)
         .exec(
@@ -107,7 +107,7 @@ app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
 // ==========================================
 // Crear un nuevo usuario
 // ==========================================
-app.post('/', mdAutenticacion.verificaToken, (req, res) => {
+app.post('/', (req, res) => {
 
     var body = req.body;
 
